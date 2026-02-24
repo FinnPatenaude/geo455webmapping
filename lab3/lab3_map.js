aad7ca08f1a9637b2b3df34dc5dab737f07e9241
FinnPatenaude <finnpatenaude@gmail.com>
2026-02-23 19:29:22 -0600

    Add files via upload

--- lab3/lab3_map.js
+++ lab3/lab3_map.js
@@ -7,17 +7,19 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 L.marker([48.14575, -88.4861])
   .addTo(map)
-  .bindPopup("<b>Welcome to Isle Royale!</b><br>This is where I started my trip; at Rock Harbor Visitor Center.")
+  .bindPopup("<b>Location #1: Rock Harbor Visitor Center</b><br>Welcome to Isle Royale! This is where most people arrive to the island. My tripmates and I started our trip here too, arriving on the Isle Royale Queen IV ferry from Copper Harbor, MI.")
   .openPopup();
 
 L.marker ([48.092001, -88.5946])
     .addTo(map)
+    .bindPopup("<b> Location #2: Daisy Farm Campground</b> <br>This is where we stayed our first night. This site reminded me a lot of my grandfather, it had an amazing rocky beach and a great dock to jump off of and read on. We revisited this campground when we were picked up by the Ranger II Ferry on Day 14.");
 
 L.marker ([48.07299, -88.6979])
     .addTo(map)
-
+    .bindPopup("");
+  
 L.marker ([48.02162, -88.8474])
-    .addTo(map)
+    .addTo(map);
 
 L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
   maxZoom: 17,
