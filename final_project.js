61894164780f5802ea0d7c9a3a6398c75cc3488d
FinnPatenaude <finnpatenaude@gmail.com>
2026-04-27 21:43:47 -0500

    Add files via upload

--- final_project.js
+++ final_project.js
@@ -1,29 +1,41 @@
 // ------------------------
 // Base map and main map
 // ------------------------
+
 var mymap = L.map("map", {
   center: [43.718490, -91.249710],
-  zoom: 9
-    //-- layers: streets ...and a comma above, might need? makes basemap go blank --//
+  zoom: 8
 });
 
-var Esri_WorldTopoMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
-});
- 
+var Esri_WorldTopoMap = L.tileLayer(
+  'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
+  {
+    attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+  }
+);
+
 // Base tile layer
-var grey = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-  attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community',
-  subdomains: 'abcd',
-  maxZoom: 20
-}).addTo(mymap);
- 
-// Mini map
-var miniLayer = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  minZoom: 0,
-  maxZoom: 13,
-  attribution: '&copy; OpenStreetMap'
-});
+var grey = L.tileLayer(
+  'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
+  {
+    attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community',
+    subdomains: 'abcd',
+    maxZoom: 20
+  }
+).addTo(mymap);
+
+// ------------------------
+// Mini Map
+// ------------------------
+
+var miniLayer = L.tileLayer(
+  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+  {
+    minZoom: 0,
+    maxZoom: 13,
+    attribution: '&copy; OpenStreetMap'
+  }
+);
 
 var miniMap = new L.Control.MiniMap(miniLayer, {
   toggleDisplay: true,
@@ -31,64 +43,115 @@ var miniMap = new L.Control.MiniMap(miniLayer, {
   position: "bottomleft"
 }).addTo(mymap);
 
-//--Icon import/creation --//
+// ------------------------
+// Icon
+// ------------------------
 
 var myIcon = L.icon({
-    iconUrl: 'imagesFP/4874bc2389e71df4c479ad933b12226a.svg',
-    iconSize: [30, 30],
-    iconAnchor: [10, 15],
-    popupAnchor: [1, -24],
+  iconUrl: 'imagesFP/4874bc2389e71df4c479ad933b12226a.svg',
+  iconSize: [30, 30],
+  iconAnchor: [10, 15],
+  popupAnchor: [1, -24],
 });
 
-//-- Variable for Peaks --//
+// ------------------------
+// Parks / Trails Layer
+// ------------------------
+
 var trail_spots = new L.geoJson(great_trail_spots, {
-  onEachFeature: function(feature, featureLayer) {
+  onEachFeature: function (feature, featureLayer) {
     featureLayer.bindPopup(
-        '<p>Park Name: <b>' + feature.properties.TITLE + '</b><br>' +
-        'City/Town: ' + feature.properties.City + ' </br>' +
-        'Park Details: ' + feature.properties.details + '</br>' +
-        'County: ' + feature.properties.county + '</p>'
-    
+      '<p><b>' + feature.properties.TITLE + '</b><br>' +
+      'City/Town: ' + feature.properties.City + '<br>' +
+      'Details: ' + feature.properties.details + '<br>' +
+      'County: ' + feature.properties.county + '</p>'
     );
   },
-  pointToLayer: function(feature, latlng) {
-    return L.marker (latlng, {icon: myIcon});
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng, { icon: myIcon });
   }
 }).addTo(mymap);
 
-var homeCenter = mymap.getCenter(); // this is extracting the original map center and saving it in the variable
+// ------------------------
+// Home Button
+// ------------------------
 
-var homeZoom = mymap.getZoom(); // this is extracting the original zoom and saving it in the variable
+var homeCenter = mymap.getCenter();
+var homeZoom = mymap.getZoom();
 
-L.easyButton(('<img src="Home_icon_blackFP.png", height=70%>'), function () {
+L.easyButton('<img src="Home_icon_blackFP.png" style="height:70%;">', function () {
   mymap.setView(homeCenter, homeZoom);
-}, "Home").addTo(mymap);
+}, "Original View").addTo(mymap);
 
-//-- search box --//
+L.easyButton('<img src="imagesFP/info_icon.png" style="width:20px;height:20px;">', function () {
+  window.location.href = "finalprojectinfo.html";
+}, "Info Page").addTo(mymap);
+
+// ------------------------
+// Heat Map
+// ------------------------
+
+var heat;
+
+function createHeatMap(data) {
+
+  var heatMapPoints = [];
+
+  data.features.forEach(function (feature) {
+    var coords = feature.geometry.coordinates;
+
+    var lat = coords[1];
+    var lng = coords[0];
+
+    heatMapPoints.push([lat, lng, 1]);
+  });
+
+  heat = L.heatLayer(heatMapPoints, {
+    radius: 25,
+    minOpacity: 0.25,
+    gradient: { 0.5: 'yellow', 0.75: 'orange', 1: 'red' }
+  });
+
+  heat.addTo(mymap);
+}
+
+// Load GeoJSON once
+fetch('baldeagles.geojson')
+  .then(response => response.json())
+  .then(data => {
+    createHeatMap(data);
+
+    // ------------------------
+    // Layer Control
+    // ------------------------
+
+    var baseMaps = {
+      "Topographic": Esri_WorldTopoMap
+    };
+
+    var overlays = {
+      "Park Locations": trail_spots,
+      "Bald Eagle Sightings": heat
+    };
+
+    L.control.layers(baseMaps, overlays, {
+      collapsed: false
+    }).addTo(mymap);
+  });
+
+// ------------------------
+// Search Control
+// ------------------------
 
 var searchControl = new L.Control.Search({
-    position:'topright',
-    layer: trail_spots,
-    propertyName: 'TITLE',
-    marker: false,
-    markeranimate: true,
-    delayType: 100,
-    collapsed: false,
-    textPlaceholder: 'Search by Park/Bird Name: e.g. Houska Park, Robin',   
-    moveToLocation: function(latlng, title, map) {
-        mymap.setView(latlng, 15);}
+  position: 'topright',
+  layer: trail_spots,
+  propertyName: 'TITLE',
+  marker: false,
+  markeranimate: true,
+  delayType: 100,
+  collapsed: false,
+  textPlaceholder: 'Search by Park Name'
 });
 
-mymap.addControl(searchControl); 
-
-//-- layer control menu --//
-
-var baseMaps = {
-  "Topographic": Esri_WorldTopoMap,
-};
-    
-var overlays = {
-  "Park Locations": trail_spots,
-};
-
-L.control.layers(baseMaps, overlays, {collapsed: false }).addTo(mymap);
+mymap.addControl(searchControl);
